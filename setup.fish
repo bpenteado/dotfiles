77a40dda4588979a5b6449ddf0d69f2251635881
Bernardo Penteado <bernardopbf3@gmail.com>
2020-10-15 15:19:05 +0100

    fixed setup script

--- setup.fish
+++ setup.fish
@@ -2,19 +2,24 @@
 
 # MacOS setup script for dotfiles
 # Clone bpenteado/dotfiles to home and run script
-
+# Warning: will overwrite existing links
+ 
 # VIM
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -si ~/dotfiles/.vimrc ~/.vimrc
 
 # skhd
-ln -s ~/dotfiles/.skhd ~/.skhd
+ln -si ~/dotfiles/.skhdrc ~/.skhdrc
 
 # yabai
-ln -s ~/dotfiles/.yabairc ~/.yabairc 
+ln -si ~/dotfiles/.yabairc ~/.yabairc 
 
 # fish
-ln -s ~/dotfiles/fish/conf.d/ ~/.config/fish/conf.d/
-ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish 
-ln -s ~/dotfiles/fish/fish_variables ~/.config/fish/fish_variables 
-ln -s ~/dotfiles/fish/fishd.80e650104292 ~/.config/fish/fishd.80e650104292 
-ln -s ~/dotfiles/fish/functions/ ~/.config/fish/functions/
+if test -d ~/.config/fish
+	ln -sin ~/dotfiles/fish/config.fish ~/.config/fish/config.fish 
+	ln -sin ~/dotfiles/fish/fishd.80e650104292 ~/.config/fish/fishd.80e650104292 
+	ln -sin ~/dotfiles/fish/fish_variables ~/.config/fish/fish_variables 
+	ln -sin ~/dotfiles/fish/functions/ ~/.config/fish/functions
+	ln -sin ~/dotfiles/fish/conf.d/ ~/.config/fish/conf.d
+else
+	echo "Could not find ~/.config/fish"
+end
