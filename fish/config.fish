003791ce323809cab319eaf182a4e6501f567452
Bernardo Penteado <bernardopbf3@gmail.com>
2020-10-21 21:45:58 +0100

    set PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,2 @@
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/bernardopenteado/Desktop/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/bernardopenteado/Desktop/google-cloud-sdk/path.fish.inc'; else; . '/Users/bernardopenteado/Desktop/google-cloud-sdk/path.fish.inc'; end; end
+# Set PATH
+set PATH $PATH /Users/bernardopenteado/anaconda/bin
